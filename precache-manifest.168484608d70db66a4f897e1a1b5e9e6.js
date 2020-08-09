8616a21ed110da6f8248f37bc8893d24c97f9172
qnguyenhuy1999 <qnguyenhuy1999@gmail.com>
2020-08-09 19:57:24 +0700

    Updates

---
+++ precache-manifest.168484608d70db66a4f897e1a1b5e9e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97199b5ca3cd76c637555b8125ed84b",
+    "url": "/typing-speed-test/index.html"
+  },
+  {
+    "revision": "d7d1d0179e9fe0c3147f",
+    "url": "/typing-speed-test/static/css/main.cf7ad822.chunk.css"
+  },
+  {
+    "revision": "c543a764bce1a450e97b",
+    "url": "/typing-speed-test/static/js/2.82a8649f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/typing-speed-test/static/js/2.82a8649f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7d1d0179e9fe0c3147f",
+    "url": "/typing-speed-test/static/js/main.8f00fb73.chunk.js"
+  },
+  {
+    "revision": "b0f30ebcd025cb189720",
+    "url": "/typing-speed-test/static/js/runtime-main.96b91f95.js"
+  }
+]);
